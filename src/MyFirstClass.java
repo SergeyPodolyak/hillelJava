9f702e115c45c78373eeba5045f8805f8b8d0a98
SergeyPodolyak <s.podolyak2013@yandex.ru>
2016-02-09 21:19:45 +0200

    firstLesson, hello world

---
+++ src/MyFirstClass.java
@@ -0,0 +1,17 @@
+/**
+ * Created by User on 09.02.2016.
+ */
+public class MyFirstClass {
+    public static void main(String args[]){
+        String message;
+        int birthYear;
+        birthYear = 1985;
+        long a=2016;
+                int age= 2016-birthYear;
+        message="Hellow" + "world, I am" + age;
+        System.out.println(age);
+       System.out.println(message);
+        long millisecondsFromBC=2016l*365l*24l*3600l*1000l;
+        System.out.println(millisecondsFromBC);
+    }
+}
