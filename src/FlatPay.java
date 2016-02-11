2b399b41ad1a797c7e2fdf778e6c56748e9e0bb4
SergeyPodolyak <s.podolyak2013@yandex.ru>
2016-02-12 01:02:28 +0200

    смисм

---
+++ src/FlatPay.java
@@ -0,0 +1,22 @@
+/**
+ * Created by ADMIN on 12.02.16.
+ */
+public class FlatPay {
+    public static void main(String[] args) {
+        int days= 4;
+        int prise=40;
+        int totalPris;
+        if (days<7&&days>3){
+            totalPris=days*prise-40;
+            System.out.println("Total prise = " + totalPris+"$");
+        }else {
+            if(days>=7){
+                totalPris=days*prise-50;
+                System.out.println("Total prise = " + totalPris+"$");
+            }else{
+                totalPris=days*prise;
+                System.out.println("Total prise = " + totalPris+"$");
+            }
+        }
+    }
+}
