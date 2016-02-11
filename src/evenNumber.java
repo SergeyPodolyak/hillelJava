8e43a7f955dae8ee86e43290a87aba601e5f995d
SergeyPodolyak <s.podolyak2013@yandex.ru>
2016-02-12 01:21:42 +0200

    смисм

---
+++ src/evenNumber.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ADMIN on 12.02.16.
+ */
+public class evenNumber {
+    public static void main(String[] args) {
+        int number = 6;
+        if (number%2>0){
+            System.out.println("Number is not event");
+        }else{System.out.println("Number is event");}
+    }
+}
