d239ddd821f7b6452e714c2a44a57f8f4c119859
SergeyPodolyak <s.podolyak2013@yandex.ru>
2016-02-23 01:01:02 +0200

    смисм

--- HomeWorkLesson3/src/Exampl2.java
+++ HomeWorkLesson3/src/Exampl2.java
@@ -10,17 +10,30 @@ public class Exampl2 {
         System.out.println("Input  werb");
         String werb = newscaner.nextLine();
         String[] werbArray = werb.split(" ");
-for (int i=0; i<werbArray.length; i++){
-    int paternLengs = werbArray[i].length()-1;
-    char first = werbArray[i].charAt(0);
-    char second = werbArray[i].charAt(paternLengs);
-    String betwen = Integer.toString(paternLengs-1);
-    werbArray[i]=first+betwen+second;
-
-}
-        for (int i=0; i<werbArray.length; i++){
-            System.out.print(werbArray[i]+" ");
+        String[] newwerbArray=getShortSymbolikOfTheWord(werbArray);
+        //
+         // for (int i=0; i<werbArray.length; i++){
+           //   int paternLengs = werbArray[i].length()-1;
+           //   char first = werbArray[i].charAt(0);
+            //  char second = werbArray[i].charAt(paternLengs);
+         //     String betwen = Integer.toString(paternLengs-1);
+           //   werbArray[i]=first+betwen+second;
+         // }
+        //
+        for (int i=0; i<newwerbArray.length; i++){
+            System.out.print(newwerbArray[i]+" ");
         }
 
+    }
+    private static String[] getShortSymbolikOfTheWord(String[] werbArray){
+String[] newWerbArray = new String[werbArray.length];
+        for (int i=0; i<werbArray.length; i++){
+            int paternLengs = werbArray[i].length()-1;
+            char first = werbArray[i].charAt(0);
+            char second = werbArray[i].charAt(paternLengs);
+            String betwen = Integer.toString(paternLengs-1);
+            newWerbArray[i]=first+betwen+second;
+        }
+        return newWerbArray;
     }
 }
--- HomeWorkLesson3/src/Exampl3.java
+++ HomeWorkLesson3/src/Exampl3.java
@@ -10,25 +10,49 @@ public class Exampl3 {
         System.out.println("Input  werb");
         String werb = newscanner.nextLine();
         String[] werbArray = werb.split(",");
-        int quantitiOfPolindroms=0;
-        for (int i=0; i<werbArray.length;i++){
-             werbArray[i]=werbArray[i].trim();
-             String tempword=werbArray[i];
-             int wordLength = werbArray[i].length();
-             int wordCentre = wordLength/2;
-             int literaequals = 0;
-             for (int j=0; j<wordCentre; j++){
-                 char firstlitera = tempword.charAt(j);
-                 char secondlitera = tempword.charAt(wordLength-j-1);
-                 if (firstlitera==secondlitera){
-                     literaequals=literaequals+1;
-                 }
-             }
-            if (literaequals==wordCentre){
-                quantitiOfPolindroms=quantitiOfPolindroms+1;
+        int quantitiOfPolindroms=kalkulationQuantityOfPolindroms(werbArray);
+        //int quantitiOfPolindroms=0;
+        //for (int i=0; i<werbArray.length;i++){
+        //   werbArray[i]=werbArray[i].trim();
+        //     String tempword=werbArray[i];
+        //     int wordLength = werbArray[i].length();
+        //     int wordCentre = wordLength/2;
+        //     int literaequals = 0;
+        //     for (int j=0; j<wordCentre; j++){
+        //         char firstlitera = tempword.charAt(j);
+        //         char secondlitera = tempword.charAt(wordLength-j-1);
+        //        if (firstlitera==secondlitera){
+        //            literaequals=literaequals+1;
+        //        }
+        //    }
+        //   if (literaequals==wordCentre){
+        //       quantitiOfPolindroms=quantitiOfPolindroms+1;
+        //   }
+
+        //}
+        System.out.println("There are " + quantitiOfPolindroms + " palindromes in the text");
+    }
+
+    private static int kalkulationQuantityOfPolindroms(String[] werbArray) {
+        int quantitiOfPolindroms = 0;
+        for (int i = 0; i < werbArray.length; i++) {
+            werbArray[i] = werbArray[i].trim();
+            String tempword = werbArray[i];
+            int wordLength = werbArray[i].length();
+            int wordCentre = wordLength / 2;
+            int literaequals = 0;
+            for (int j = 0; j < wordCentre; j++) {
+                char firstlitera = tempword.charAt(j);
+                char secondlitera = tempword.charAt(wordLength - j - 1);
+                if (firstlitera == secondlitera) {
+                    literaequals = literaequals + 1;
+                }
+            }
+            if (literaequals == wordCentre) {
+                quantitiOfPolindroms = quantitiOfPolindroms + 1;
             }
 
         }
-        System.out.println("There are "+quantitiOfPolindroms+" palindromes in the text");
+        return quantitiOfPolindroms;
     }
 }
