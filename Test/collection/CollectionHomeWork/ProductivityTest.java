f27fe20b84c73139c56bdcc84d9f756bbb3945b8
SergeyPodolyak <s.podolyak2013@yandex.ru>
2016-03-28 01:57:44 +0300

    смисм

---
+++ Test/collection/CollectionHomeWork/ProductivityTest.java
@@ -0,0 +1,27 @@
+package collection.CollectionHomeWork;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+/**
+ * Created by ADMIN on 28.03.16.
+ */
+public class ProductivityTest {
+    @Test
+    public void addEllementsMustBeNormal(){
+        LinkedList experimentalLinkedList = new LinkedList();
+        Productivity.addToOurElement(experimentalLinkedList);
+        Integer a =new Integer(1);
+        Assert.assertEquals(a,experimentalLinkedList.get(1));
+    }
+    @Test
+    public void chekSizeOfLinkedList(){
+        LinkedList experimentalLinkedList = new LinkedList();
+        Productivity.addToOurElement(experimentalLinkedList);
+
+        Assert.assertEquals(1000000,experimentalLinkedList.size());
+    }
+    
+}
---
+++ src/collection/CollectionHomeWork/Productivity.java
@@ -0,0 +1,70 @@
+package collection.CollectionHomeWork;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+
+/**
+ * Created by ADMIN on 28.03.16.
+ */
+public class Productivity {
+    public static void main(String[] args) {
+        ArrayList experimentalArrayList = new ArrayList();
+        LinkedList experimentalLinkedList = new LinkedList();
+        Collection experimentalTreeSet = new TreeSet();
+
+       // addToOurElement(experimentalArrayList);
+        //System.out.println("Time of add element in the begin to Array List " + addInTheBegining(experimentalArrayList));
+
+       // System.out.println("Time of add element in the end to Array List " + addInTheEnd(experimentalArrayList));
+        addToOurElement(experimentalLinkedList);
+        System.out.println("Time of add element in the begin to Linked List " + addInTheBeginingLinkedList(experimentalLinkedList));
+
+        System.out.println("Time of add element in the end to Linked List " + addInTheEndLinkedList(experimentalLinkedList));
+    }
+
+    public static void addToOurElement(Collection experimental) {
+        for (int i = 0; i < 1000000; i++) {
+            Integer a = new Integer(i);
+            experimental.add(a);
+        }
+    }
+
+    public static long addInTheBegining(ArrayList experimental) {
+        Integer a = new Integer(5);
+        Instant begin = Instant.now();
+        experimental.add(1, a);
+        Instant end = Instant.now();
+        Duration duration = Duration.between(begin, end);
+        return duration.toMillis();
+    }
+
+    public static long addInTheEnd(ArrayList experimental) {
+        Integer a = new Integer(5);
+        Instant begin = Instant.now();
+        experimental.add(100, a);
+        Instant end = Instant.now();
+        Duration duration = Duration.between(begin, end);
+        return duration.toMillis();
+
+    }
+
+    public static long addInTheBeginingLinkedList(LinkedList experimental) {
+        Integer a = new Integer(5);
+        Instant begin = Instant.now();
+        experimental.add(1, a);
+        Instant end = Instant.now();
+        Duration duration = Duration.between(begin, end);
+        return duration.toMillis();
+    }
+    public static long addInTheEndLinkedList(LinkedList experimental) {
+        Integer a = new Integer(5);
+        Instant begin = Instant.now();
+        experimental.add(9999999, a);
+        Instant end = Instant.now();
+        Duration duration = Duration.between(begin, end);
+        return duration.toMillis();
+
+    }
+
+}
